a083baddd853514a5697e3a98eea4251c2ce5487
elmonitor <info@elmonitor.net>
2016-10-19 15:08:17 -0500

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Product price based on margin with formula sale_price=cost_price/margin",
+    "version": "8.0.0.1",
+    "author": "3nodus",
+    'category': 'Product',
+    "website": "http://www.3nodus.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+    ],
+    "demo": [
+
+    ],
+    "data": [
+        
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
