4c772ddda1bd9015c0059a9472445a2859bc037f
elmonitor <info@elmonitor.net>
2016-10-19 15:51:24 -0500

    Update __openerp__.py

--- product_price_margin_div/__openerp__.py
+++ product_price_margin_div/__openerp__.py
@@ -1,6 +1,5 @@
 {
     "name": "Product price based on margin",
-    "summary": "Product price based on margin with formula (sale_price=cost_price/margin)",
     "version": "8.0.0.1",
     "author": "3nodus",
     'category': 'Product',
